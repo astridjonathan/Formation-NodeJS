68e1d1e8ecf5940b293b5815dc3c188a501e0c25
Astrid JONATHAN <astrid.jonathan@gmail.com>
2019-09-24 15:11:31 -0400

    w du 2409
    finalisation serveur web
    url
    fs

---
+++ exoJS/program.js
@@ -0,0 +1,12 @@
+// var sum = 0;
+// for (i = 2; i < process.argv.length; i++) {
+//     sum += Number(process.argv[i]);
+// }
+//
+// console.log(sum);
+
+const fs = require ('fs');
+const str = buf.toString();
+
+
+
--- server-node.js
+++ server-node.js
@@ -14,14 +14,88 @@ const http = require('http');
  **/
 const hostname = '127.0.0.1';
 const port = 3000;
+
+/** Permet d'importer le module 'url'
+ * qui permet de lire l'url et ses données
+* @type{module:url}
+*
+**/
+const url = require('url');
+
+/** Importe le module 'fs'
+ * qui permet d'accéder au système de fichier du pc
+ * @type{module:fs}
+ *
+ **/
+const fs = require ('fs');
+
 /**
  * http.createServer([options][, requestlistener = request and response])
  * statusCode = 200 => code de succes
  * */
 const server = http.createServer((req, res) => {
-    res.statusCode= 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World 4.0 !\n');
+    let path=url.parse(req.url).pathname;
+    console.log(path);
+    console.log(__dirname);
+
+    switch (path)
+        {
+            case ('/') :
+                {
+                    fs.readFile(__dirname + '/views/html/index.html', (err , data)=>{
+                        /**
+                         * le contenu de ma fonciton ne sera exécuté que lorque nodeJS aura fini
+                         * la lecture de mon fichier
+                         *
+                         * data contient des données de ma page html
+                         * */
+                        // En cas d'erreur je l'affiche dans ma console
+                        if (err) console.log(err);
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'text/html; charset="utf-8');
+                        res.end(data);
+                    }); // de fs.readFile
+
+
+
+                    break;
+                }
+            case ('/contacts'):
+                {
+                    fs.readFile(__dirname + '/views/html/contacts.html', (err , data)=>{
+
+
+                        if (err) console.log(err);
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'text/html; charset="utf-8');
+                        res.end(data);
+                    }); // de fs.readFile
+                    break;
+                }
+            case ('/contact'):
+            {
+                let params= url.parse(req.url, true).query;
+                let firstname = params.firstname || 'Anonymous';
+                let lastname = params.lastname || '';
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html; charset="utf-8');
+                res.end(`
+                    <html><body><h1>Mon contact : </h1></body></html>
+                    `);
+                break;
+            }
+            default :
+                {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html; charset="utf-8');
+                    res.end(`
+                    <html><body><h1>Oupsss Erreur 404</h1></body></html>
+                    `);
+                    break;
+                }
+        }
+
 });
 /***
  *
